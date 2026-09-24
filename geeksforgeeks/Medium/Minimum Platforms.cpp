1db454fed7239b55d09c3e5d6f61fee1cf846c08
DhruvX-J <officialdhruvsharma18@gmail.com>
2026-09-25 00:31:45 +0530

    Add Minimum Platforms (Medium)

---
+++ geeksforgeeks/Medium/Minimum Platforms.cpp
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int minPlatform(int[] arr, int[] dep) {
+        int n = arr.length;
+
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+
+        int i = 0;
+        int j = 0;
+
+        int platforms = 0;
+        int maxPlatforms = 0;
+
+        while (i < n && j < n) {
+
+            // Arrival happens before or at departure
+            if (arr[i] <= dep[j]) {
+                platforms++;
+                maxPlatforms = Math.max(maxPlatforms, platforms);
+                i++;
+            } else {
+                platforms--;
+                j++;
+            }
+        }
+
+        return maxPlatforms;
+    }
+}
+
+
