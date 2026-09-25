28b96aa312670a1cb57cee6af288a0a8c1b63f09
DhruvX-J <officialdhruvsharma18@gmail.com>
2026-09-26 01:09:32 +0530

    Add Minimum Cost Pizza Selection (Medium)

---
+++ geeksforgeeks/Medium/Minimum Cost Pizza Selection.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimumCost(int x, int s, int m, int l, int cs, int cm, int cl) {
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= x / s + 1; i++) {
+            for (int j = 0; j <= x / m + 1; j++) {
+
+                int area = i * s + j * m;
+
+                int k = 0;
+
+                if (area < x) {
+                    k = (x - area + l - 1) / l;
+                }
+
+                int cost = i * cs + j * cm + k * cl;
+
+                ans = Math.min(ans, cost);
+            }
+        }
+
+        return ans;
+    }
+}
