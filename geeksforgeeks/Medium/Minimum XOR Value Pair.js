729790e75f40016204f668d32477ac6b47a84858
DhruvX-J <officialdhruvsharma18@gmail.com>
2026-09-27 01:08:32 +0530

    Add Minimum XOR Value Pair (Medium)

---
+++ geeksforgeeks/Medium/Minimum XOR Value Pair.js
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int minXor(int[] arr) {
+        Arrays.sort(arr);
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 1; i < arr.length; i++) {
+            ans = Math.min(ans, arr[i] ^ arr[i - 1]);
+        }
+
+        return ans;
+    }
+}
